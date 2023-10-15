7673dc0764a87740f430d1839569d1b8e2496889
Subr1ata <subrotomukherjee8@gmail.com>
2023-10-15 12:28:20 +0530

    logo and transition added to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,19 @@
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
 import React from 'react'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='flex justify-between p-3 px-10 border-b-[1px] shadow-sm'>
+      <div className='flex gap-10 items-center'>
+        <Image src='/logo.png' height={60} width={120} className='w-40 h-120' alt='logo' priority />
+        <div className='hidden md:flex gap-6'>
+          <h2 className='hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all'>Home</h2>
+          <h2 className='hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all'>History</h2>
+          <h2 className='hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all'>Help</h2>
+        </div>
+      </div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
       <h2>Subscribe to Tubeguruji</h2>
     </div>
   )
